44c4d9533d7d600fd9735bce563cfe64f534b07a
YiMSIT54 <wattanachai.dev@gmail.com>
2020-01-19 11:21:34 +0700

    Camera And Gallery Ok

--- lib/widget/register.dart
+++ lib/widget/register.dart
@@ -1,4 +1,6 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:smiletravle/utility/my_style.dart';
 
 class Register extends StatefulWidget {
@@ -8,6 +10,7 @@ class Register extends StatefulWidget {
 
 class _RegisterState extends State<Register> {
   // Field
+  File file;
 
   // Method
 
@@ -113,15 +116,35 @@ class _RegisterState extends State<Register> {
     return OutlineButton.icon(
       icon: Icon(Icons.add_a_photo),
       label: Text('Camera'),
-      onPressed: () {},
+      onPressed: () {
+        cameraOrGallery(ImageSource.camera);
+      },
     );
+  }
+
+  Future<void> cameraOrGallery(ImageSource imageSource) async {
+    try {
+      var object = await ImagePicker.pickImage(
+        source: imageSource,
+        maxWidth: 800.0,
+        maxHeight: 800.0,
+      );
+
+      setState(() {
+        file = object;
+      });
+    } catch (e) {
+      print('e ===>>> ${e.toString()}');
+    }
   }
 
   Widget galleryButton() {
     return OutlineButton.icon(
       icon: Icon(Icons.add_photo_alternate),
       label: Text('Gallery'),
-      onPressed: () {},
+      onPressed: () {
+        cameraOrGallery(ImageSource.gallery);
+      },
     );
   }
 
@@ -139,7 +162,7 @@ class _RegisterState extends State<Register> {
     return Container(
       width: MediaQuery.of(context).size.width,
       height: MediaQuery.of(context).size.height * 0.5,
-      child: Image.asset('images/avatar.png'),
+      child: file == null ? Image.asset('images/avatar.png') : Image.file(file),
     );
   }
 
