f0ebe0cf39a41d7cee5fa49d3189564da2060d98
Wattanachai Waknang <37531787+YiMSIT54@users.noreply.github.com>
2020-10-13 04:20:45 +0700

    ..

--- lib/widget/auth.dart
+++ lib/widget/auth.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:smiletravle/utility/my_style.dart';
 import 'package:smiletravle/utility/normal_dialog.dart';
